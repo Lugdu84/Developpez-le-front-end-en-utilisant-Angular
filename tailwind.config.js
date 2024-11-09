75dcd4e1fced1fb3c3139cd27d441c13dc682cbd
David Grammatico <david.lugdunum@gmail.com>
2024-11-09 17:15:43 +0100

    add custom primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0b86cd",
+      },
+    },
   },
   plugins: [],
 };
