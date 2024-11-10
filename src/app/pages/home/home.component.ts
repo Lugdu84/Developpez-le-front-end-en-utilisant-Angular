01bd7e385c2d5f46628cd5e71d30670f0a4db693
David Grammatico <david.lugdunum@gmail.com>
2024-11-10 19:21:04 +0100

    add signals for numberOfCountries and numberOfJo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal, WritableSignal } from '@angular/core';
 import { map, Observable, of, pipe, tap } from 'rxjs';
 import { OlympicService } from '@services/olympic.service';
 import { Olympics } from '@models/Olympic';
@@ -11,15 +11,25 @@ import { Charts } from '@models/Chart';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  numberOfCountries!: number;
-  numberOfJo!: number;
-  public olympics$: Observable<Charts> = of([]);
+  protected numberOfCountries = signal(0);
+  protected numberOfJo = signal(0);
+  protected olympics$: Observable<Charts> = of([]);
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics().pipe(
-      tap((value) => console.log(value)),
+      tap((olympics) => {
+        this.numberOfCountries.set(olympics.length);
+        this.numberOfJo.set(
+          olympics.reduce(
+            (acc, olympic) =>
+              acc +
+              olympic.participations.filter((p) => p.medalsCount > 0).length,
+            0
+          )
+        );
+      }),
       map((olympics) =>
         olympics.map((olympic) => ({
           name: olympic.country,
